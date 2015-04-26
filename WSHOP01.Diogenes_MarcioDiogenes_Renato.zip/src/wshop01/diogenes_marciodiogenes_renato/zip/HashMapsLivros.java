b12f5e937b343d4ef6e9becb76fff85a7d7f7324
Marushio <marcio.olivercross@gmail.com>
2015-04-26 20:54:04 -0300

    comentarios adicionados

--- WSHOP01.Diogenes_MarcioDiogenes_Renato.zip/src/wshop01/diogenes_marciodiogenes_renato/zip/HashMapsLivros.java
+++ WSHOP01.Diogenes_MarcioDiogenes_Renato.zip/src/wshop01/diogenes_marciodiogenes_renato/zip/HashMapsLivros.java
@@ -13,19 +13,20 @@ import java.util.Scanner;
  * @author Marcio
  */
 public class HashMapsLivros {
+    //declara o hashmape seus parametors
     private HashMap<Integer, Livro> mapeamentoDeLivros; 
     Scanner entrada=new Scanner(System.in);
     public HashMapsLivros(){ 
+        //cria uma novo hasmap
         mapeamentoDeLivros = new HashMap<Integer, Livro>(); 
     }
     public void cadastrarNovoLivro(){  
-        
-         
-        
+
         Livro livro = new Livro();
         System.out.println( "Digite os dados do novo livro: ");
         System.out.println( "Numero ISBN: ");
         livro.setIsbn(entrada.nextInt());
+        //metodo containsKey do hash map, verifica se um indice ja existe 
         if(mapeamentoDeLivros.containsKey(livro.getIsbn())){
             System.out.println("Erro: Numero ISBN Não encontrado!");   
         }else{
@@ -35,6 +36,7 @@ public class HashMapsLivros {
             livro.setAutor(entrada.next());
             System.out.println( "Ano :");
             livro.setAno(entrada.nextInt());
+            //metodo put do hash map, insere um novo valor nas lista 
             mapeamentoDeLivros.put(livro.getIsbn(), livro);  
         }      
     }     
@@ -44,10 +46,10 @@ public class HashMapsLivros {
         int valorPsquisa = entrada.nextInt();
  
         try {  
-             
+             //metodo containsKey do hash map, verifica se um indice existe 
             if( !mapeamentoDeLivros.containsKey(valorPsquisa) )  
                 throw new Exception();  
-              
+            // metodo get(valor indice)ja pega o objeto  no indice passado como parametro  
             System.out.println( "ISBN:   "+ mapeamentoDeLivros.get(valorPsquisa).getIsbn());  
             System.out.println( "Titulo: "+ mapeamentoDeLivros.get(valorPsquisa).getTitulo());  
             System.out.println( "Autor:  "+ mapeamentoDeLivros.get(valorPsquisa).getAutor());  
